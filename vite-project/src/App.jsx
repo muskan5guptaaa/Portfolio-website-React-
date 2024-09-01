bb96963dcfba85e63d06bad13482bc984dcad6a1
muskan5guptaaa <165368032+muskan5guptaaa@users.noreply.github.com>
2024-09-01 20:38:06 +0530

    commit

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './App.css';
+import styled, { ThemeProvider } from "styled-components";
+import {darkTheme} from "./utils/Themes"
+
+
+const Body = styled.div`
+  background-color: ${({ theme }) => theme.bg};
+  width: 100%;
+  overflow-x: hidden;
+`
+function App() {
+  return (
+
+<ThemeProvider theme={darkTheme} >
+  <Body>Portfolio</Body>
+</ThemeProvider> )
+}
+
+export default App
